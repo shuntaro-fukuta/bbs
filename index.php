c187d2c5243c1665e392e456072d125ffef0f2ea
Hayatarou921 <Hayatarou921@gmail.com>
2019-10-03 10:35:54 +0900

    テキストエリアと投稿ボタンの作成

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
 
-echo "hello world";
+?>
 
-
+<html>
+  <head>
+    <title>challnege1</title>
+  </head>
+  <body>
+    <form>
+      <textarea>
+      </textarea>
+      <br>
+      <input type="submit" value="Submit">
+    </form>
+  </body>
+</html>
