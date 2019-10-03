22c712de09abe65bfe0f479c9fbe4b1bd5bfbd8a
Hayatarou921 <Hayatarou921@gmail.com>
2019-10-03 16:37:57 +0900

    バリデーション機能を実装

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
     $password = 'root';
     $db_name  = 'bbs';
 
+    $max_comment_length = 200;
+    $min_comment_length = 10;
+
     $mysqli = new mysqli ($host, $username, $password, $db_name);
 
     if ($mysqli->connect_error) {
@@ -14,8 +17,16 @@
     $mysqli->set_charset('utf8');
 
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $comment = $mysqli->real_escape_string($_POST['comment']);
-        $mysqli->query("INSERT INTO posts (comment) VALUES ('$comment')");
+        $comment        = $mysqli->real_escape_string($_POST['comment']);
+        $comment_length = mb_strlen($comment);
+
+        if ($comment_length === 0) {
+            $error_massage = '何か入力してください。';
+        } elseif ($comment_length < $min_comment_length || $comment_length > $max_comment_length) {
+            $error_massage = '入力は10文字以上200文字以内にして下さい。';
+        } else {
+            $mysqli->query("INSERT INTO posts (comment) VALUES ('$comment')");
+        }
     }
 
     $mysqli->close();
@@ -27,6 +38,9 @@
     <title>challnege1</title>
   </head>
   <body>
+    <?php if (isset($error_massage)) : ?>
+      <?php echo $error_massage ?>
+    <?php endif ?>
     <form method="post" action="">
       <textarea name="comment"></textarea>
       <br>
