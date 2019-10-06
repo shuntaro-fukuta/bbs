1eaa3bc3db45f5efea3c1cde57dfa8fe8e7d4b14
Hayatarou921 <Hayatarou921@gmail.com>
2019-10-06 11:50:52 +0900

    指摘部分の修正

--- index.php
+++ index.php
@@ -1,83 +1,76 @@
 <?php
 
-// ebine
-// 長いコメントとかタイトルとか入力して、
-// バリデーションエラーが発生した場合、
-// 頑張って書いた内容が全部消えちゃうので、残念な気持ちになる。
+function mb_trim($string) {
+    return preg_replace('/\A[\p{Z}]+|[\p{Z}]+\z/u', '', $string);
+}
 
-    function mb_trim($string) {
-        return preg_replace('/\A[\p{Z}]+|[\p{Z}]+\z/u', '', $string);
+function h($string) {
+    return htmlspecialchars($string, ENT_QUOTES);
+}
+
+$host     = 'localhost';
+$username = 'root';
+$password = 'root';
+$db_name  = 'bbs';
+$encoding = 'utf8';
+
+$mysqli = new mysqli($host, $username, $password, $db_name);
+
+$min_title_length = 10;
+$max_title_length = 32;
+
+$min_comment_length = 10;
+$max_comment_length = 200;
+
+if ($mysqli->connect_error) {
+    echo $mysqli->connect_error;
+    exit;
+}
+
+$mysqli->set_charset($encoding);
+
+$error_massages = [];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['title'])) {
+        $title        = mb_trim($_POST['title']);
+        $title_length = mb_strlen($title);
+
+        if ($title_length === 0) {
+            $error_massages[] = 'タイトルを入力してください';
+        } elseif ($title_length < $min_title_length || $title_length > $max_title_length) {
+            $error_massages[] = "タイトルは{$min_title_length}文字以上{$max_title_length}以内で入力してください";
+        }
     }
 
-    function h($string) {
-        return htmlspecialchars($string, ENT_QUOTES);
+    if (isset($_POST['comment'])) {
+        $comment        = mb_trim($_POST['comment']);
+        $comment_length = mb_strlen($comment);
+
+        if ($comment_length === 0) {
+            $error_massages[] = 'メッセージを入力してください。';
+        } elseif ($comment_length < $min_comment_length || $comment_length > $max_comment_length) {
+            $error_massages[] = "メッセージは{$min_comment_length}文字以上{$max_comment_length}文字以内で入力してください";
+        }
     }
 
-    $host     = 'localhost';
-    $username = 'root';
-    $password = 'root';
-    $db_name  = 'bbs';
-    $encoding = 'utf8';
-
-    $mysqli = new mysqli($host, $username, $password, $db_name);
-
-    $min_title_length = 10;
-    $max_title_length = 32;
-
-    $min_comment_length = 10;
-    $max_comment_length = 200;
-
-    if ($mysqli->connect_error) {
-        echo $mysqli->connect_error;
+    if (empty($error_massages)) {
+        $title   = $mysqli->real_escape_string($title);
+        $comment = $mysqli->real_escape_string($comment);
+        $mysqli->query("INSERT INTO posts (title, comment) VALUES ('$title', '$comment')");
+        header("Location: {$_SERVER['SCRIPT_NAME']}");
         exit;
+    } else {
+        session_start();
+        $_SESSION['title']   = $title;
+        $_SESSION['comment'] = $comment;
     }
+}
 
-    $mysqli->set_charset($encoding);
+$results = $mysqli->query("SELECT * FROM posts ORDER BY created_at DESC");
+$posts   = $results->fetch_all(MYSQLI_ASSOC);
 
-    $error_massages = [];
-
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        if (isset($_POST['title'])) {
-            $title        = mb_trim($_POST['title']);
-            $title_length = mb_strlen($title);
-
-            if ($title_length === 0) {
-                $error_massages[] = 'タイトルを入力してください';
-            } elseif ($title_length < $min_title_length || $title_length > $max_title_length) {
-                $error_massages[] = "タイトルは{$min_title_length}文字以上{$max_title_length}以内で入力してください";
-            }
-        } else {
-            // ebine
-            // これなに？いらないよね
-            $error_massages[] = 'タイトルの投稿に失敗しました';
-        }
-
-        if (isset($_POST['comment'])) {
-            $comment        = mb_trim($_POST['comment']);
-            $comment_length = mb_strlen($comment);
-
-            if ($comment_length === 0) {
-                $error_massages[] = 'メッセージを入力してください。';
-            } elseif ($comment_length < $min_comment_length || $comment_length > $max_comment_length) {
-                $error_massages[] = "メッセージは{$min_comment_length}文字以上{$max_comment_length}文字以内で入力してください";
-            }
-        } else {
-            $error_massages[] = 'メッセージの投稿に失敗しました';
-        }
-
-        if (empty($error_massages)) {
-            $title   = $mysqli->real_escape_string($title);
-            $comment = $mysqli->real_escape_string($comment);
-            $mysqli->query("INSERT INTO posts (title, comment) VALUES ('$title', '$comment')");
-            header("Location: {$_SERVER['SCRIPT_NAME']}");
-            exit;
-        }
-    }
-
-    $results = $mysqli->query("SELECT * FROM posts ORDER BY created_at DESC");
-    $posts   = $results->fetch_all(MYSQLI_ASSOC);
-
-    $mysqli->close();
+$mysqli->close();
 
 ?>
 
@@ -94,9 +87,9 @@
     <?php endif ?>
     <form method="post" action="">
       <label for="title">Title</label><br>
-      <input id="title" type="text" name="title"><br>
+      <input id="title" type="text" name="title" value="<?php echo isset($_SESSION['title']) ? $_SESSION['title'] : '' ?>"><br>
       <label for="comment">Body</label><br>
-      <textarea id="comment" name="comment"></textarea><br>
+      <textarea id="comment" name="comment"><?php echo isset($_SESSION['comment']) ? $_SESSION['comment'] : '' ?></textarea><br>
       <input type="submit" value="Submit">
     </form>
     <?php foreach ($posts as $post) : ?>
@@ -107,4 +100,6 @@
       <?php echo h($post['created_at']) ?>
     <?php endforeach ?>
   </body>
+  <?php session_unset() ?>
+  <?php session_destroy() ?>
 </html>
