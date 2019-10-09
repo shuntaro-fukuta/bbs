3a22de993a6728df41485925e288fd2e6a5b7f11
Hayatarou921 <Hayatarou921@gmail.com>
2019-10-09 14:23:06 +0900

    バリデーション関数の修正

--- validations.php
+++ validations.php
@@ -15,20 +15,20 @@ function execute_validations($validation_settings, $inputs) {
 
             switch ($validation_type) {
                 case 'required':
-                    if ($condition === true && (empty($input) || is_null($input))) {
-                        $error_massages[] = "{$attribute_name}を入力してください";
+                    $error_massage = validate_input_required($attribute_name, $input, $condition);
+
+                    if (isset($error_massage)) {
+                        $error_massages[] = $error_massage;
                         continue 3;
                     }
 
                     break;
                 case 'length':
-                    if (!is_null($input)) {
-                        $error_massage = validate_input_length($attribute_name, $input, $condition);
+                    $error_massage = validate_input_length($attribute_name, $input, $condition);
 
-                        if (isset($error_massage)) {
-                            $error_massages[] = $error_massage;
-                            continue 3;
-                        }
+                    if (isset($error_massage)) {
+                        $error_massages[] = $error_massage;
+                        continue 3;
                     }
 
                     break;
@@ -40,7 +40,21 @@ function execute_validations($validation_settings, $inputs) {
     return $error_massages;
 }
 
+function validate_input_required($attribute_name, $input, $condition) {
+    $error_massage = null;
+
+    if ($condition === true && (empty($input) || is_null($input))) {
+        $error_massage = "{$attribute_name}を入力してください";
+    }
+
+    return $error_massage;
+}
+
 function validate_input_length($attribute_name, $input, $length_limits) {
+    if (is_null($input)) {
+        return;
+    }
+
     $input_length = mb_strlen($input);
 
     $error_massage = null;
