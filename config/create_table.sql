54b58e574bde1070c2c345fc12c6f9ca06798e65
Hayatarou921 <Hayatarou921@gmail.com>
2019-12-24 17:12:51 +0900

    membersテーブルを作成

--- config/create_table.sql
+++ config/create_table.sql
@@ -9,3 +9,11 @@
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=8726 DEFAULT CHARSET=utf8 |
+
+| members | CREATE TABLE `members` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 |
