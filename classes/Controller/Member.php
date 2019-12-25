f62f051f4e6f5d69839b80d8b7f4d4a0d63836d8
Hayatarou921 <Hayatarou921@gmail.com>
2019-12-25 11:31:27 +0900

    registerメソッドをざっくり実装

--- classes/Controller/Member.php
+++ classes/Controller/Member.php
@@ -5,24 +5,35 @@ class Controller_Member  extends Controller_Base
 
     public function register()
     {
-        // GET
-        // トークンがない
-        //     登録フォーム
-        // トークンがある
-        //     トークンチェックok
-        //         memberテーブルに保存
-        //         prememberから削除
-        //             登録完了画面
+        $request = $this->getEnv('request-method');
 
-        // POST
-        // 確認ボタン
-        //     validation ok
-        //         確認画面
+        if ($request === 'GET') {
+            $token = $this->getParam('token');
 
-        // submitボタン
-        //     トークン作成
-        //     pre_memberテーブルに情報を保存
-        //         メールを送る url='register.php?token=トークン'
-        //             メール送った画面
+            if (empty($token)) {
+                // render 登録フォーム
+            } else {
+                //     トークンチェックok
+                //         memberテーブルに保存
+                //         prememberから削除
+                //             登録完了画面
+            }
+        }
+
+        if ($request === 'POST') {
+            $is_confirm = $this->getParam('confirm');
+            $is_submit  = $this->getParam('submit');
+
+            if ($is_confirm) {
+                // バリデーション
+            }
+
+            if ($is_submit) {
+                //     トークン作成
+                //     pre_memberテーブルに情報を保存
+                //         メールを送る url='register.php?token=トークン'
+                //             メール送った画面
+            }
+        }
     }
 }
