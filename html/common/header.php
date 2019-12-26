91eee00e94ef9f00edebf1d993e61e2514d4135d
Hayatarou921 <Hayatarou921@gmail.com>
2019-12-26 18:21:29 +0900

    login/logout/registerボタンを追加

--- html/common/header.php
+++ html/common/header.php
@@ -3,3 +3,10 @@
     <title>challnege8</title>
   </head>
   <body>
+    <?php if (isset($_SESSION['member_id'])) : ?>
+      <input type="button" value="logout" onclick="location.href='logout.php'">
+    <?php else : ?>
+      <input type="button" value="login" onclick="location.href='login.php'">
+      <input type="button" value="register" onclick="location.href='register.php'">
+    <?php endif ?>
+    <br>
