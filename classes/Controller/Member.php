c9abe2bd1b5fd690c76dee4dd152a01d56a0e6d0
Hayatarou921 <Hayatarou921@gmail.com>
2019-12-26 14:11:09 +0900

    アカウントの認証部分を別のメソッドに分割

--- classes/Controller/Member.php
+++ classes/Controller/Member.php
@@ -4,85 +4,92 @@ class Controller_Member  extends Controller_Base
 {
     public function register()
     {
-        $request_method = $this->getEnv('request-method');
-
         // クリックジャッキング
         // csrf
+        $request_method = $this->getEnv('request-method');
+
+        if ($request_method === 'GET') {
+            $this->render('member/register/form.php');
+            return;
+        }
 
         $member    = new Storage_Member();
         $premember = new Storage_Premember();
 
-        $error_messages = [];
+        $name     = $this->getParam('name');
+        $email    = $this->getParam('email');
+        $password = $this->getParam('password');
 
-        if ($request_method === 'GET') {
-            $token = $this->getParam('token');
+        $inputs = [
+            'name'     => $name,
+            'email'    => $email,
+            'password' => $password,
+        ];
 
-            if (!empty($token)) {
-                $account = $premember->selectRecord(['*'], [['token', '=', $token]]);
-                if (empty($account)) {
-                    $error_messages[] = 'アカウントが見つかりませんでした。' . PHP_EOL . 'もう一度登録し直してください。';
-                    $this->render('member/register/form.php');
-                    return;
-                }
-
-                if (!$premember->isExpired($account['date'])) {
-                    $member->insert([
-                        'name'     => $account['name'],
-                        'email'    => $account['email'],
-                        'password' => $account['password'],
-                    ]);
-
-                    $premember->delete([['id', '=', $account['id']]]);
-
-                    $this->render('member/register/complete.php');
-                    return;
-                }
+        $error_messages = $member->validate($inputs);
+        if (empty($error_messages)) {
+            if ($this->getParam('do_confirm') === '1') {
+                $hidden_pass = str_repeat('*', strlen($inputs['password']));
+                $this->render('member/register/confirm.php', get_defined_vars());
+                return;
             }
-        }
 
-        if ($request_method === 'POST') {
-            $name     = $this->getParam('name');
-            $email    = $this->getParam('email');
-            $password = $this->getParam('password');
+            if ($this->getParam('do_register') === '1') {
+                $inputs['password'] = password_hash($inputs['password'], PASSWORD_BCRYPT);
 
-            $inputs = [
-                'name'     => $name,
-                'email'    => $email,
-                'password' => $password,
-            ];
+                $token           = uniqid(create_random_string(30), true);
+                $inputs['token'] = $token;
 
-            $error_messages = $member->validate($inputs);
-            if (empty($error_messages)) {
-                if ($this->getParam('do_confirm') === '1') {
-                    $hidden_pass = str_repeat('*', strlen($inputs['password']));
-                    $this->render('member/register/confirm.php', get_defined_vars());
-                    return;
-                }
+                $premember->insert($inputs);
 
-                if ($this->getParam('do_register') === '1') {
-                    $inputs['password'] = password_hash($inputs['password'], PASSWORD_BCRYPT);
+                $to      = $email;
+                $subject = 'アカウント登録はまだ完了しておりません';
+                $url     = 'http://centos7-amp7/authenticate.php?token=' . $token;
+                $message = 'Hi, Mr.' . $name
+                            . PHP_EOL . 'アカウント登録を完了するために、２４時間以内に以下のリンクをクリックしてください。'
+                            . PHP_EOL . $url;
+                $header  = 'From:BBS';
+                mb_send_mail($to, $subject, $message, $header);
 
-                    $token           = uniqid(create_random_string(30), true);
-                    $inputs['token'] = $token;
-
-                    $premember->insert($inputs);
-
-                    $to      = $email;
-                    $subject = 'アカウント登録はまだ完了しておりません';
-                    $url     = 'http://centos7-amp7/register.php?token=' . $token;
-                    $message = 'Hi, Mr.' . $name
-                               . PHP_EOL . 'アカウント登録を完了するために、２４時間以内に以下のリンクをクリックしてください。'
-                               . PHP_EOL . $url;
-                    $header  = 'From:BBS';
-                    mb_send_mail($to, $subject, $message, $header);
-
-                    $this->render('member/register/sent_email.php');
-                    return;
-                }
-
+                $this->render('member/register/sent_email.php');
+                return;
             }
         }
 
         $this->render('member/register/form.php', get_defined_vars());
+    }
+
+    public function authenticate()
+    {
+        if ($this->getEnv('request-method') !== 'GET') {
+            $this->err400();
+        }
+
+        $member    = new Storage_Member();
+        $premember = new Storage_Premember();
+
+        $token = $this->getParam('token');
+        if (is_null($token)) {
+            $this->err400();
+        }
+
+        $account = $premember->selectRecord(['*'], [['token', '=', $token]]);
+        if (empty($account)) {
+            $error_messages = ['会員登録に失敗しました。' . PHP_EOL . 'もう一度登録し直してください。'];
+            $this->render('member/register/form.php', get_defined_vars());
+            return;
+        }
+
+        if (!$premember->isExpired($account['date'])) {
+            $member->insert([
+                'name'     => $account['name'],
+                'email'    => $account['email'],
+                'password' => $account['password'],
+            ]);
+
+            $premember->delete([['id', '=', $account['id']]]);
+
+            $this->render('member/register/complete.php');
+        }
     }
 }
