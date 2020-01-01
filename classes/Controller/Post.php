de9d18592c81db298ae3628b494704219a094d4c
Hayatarou921 <Hayatarou921@gmail.com>
2020-01-01 16:53:35 +0900

    $is_logged_inの判定の仕方を変更

--- classes/Controller/Post.php
+++ classes/Controller/Post.php
@@ -14,7 +14,7 @@ class Controller_Post extends Controller_Base
     public function index()
     {
         $member_id    = $this->session_manager->getParam('member_id');
-        $is_logged_in = (is_null($member_id)) ? false : true;
+        $is_logged_in = ($member_id !== null);
         if ($is_logged_in) {
             $member      = new Storage_Member();
             $member_name = $member->selectRecord(['name'], [['id', '=', $member_id]])['name'];
@@ -40,7 +40,7 @@ class Controller_Post extends Controller_Base
     public function post()
     {
         $member_id    = $this->session_manager->getParam('member_id');
-        $is_logged_in = (is_null($member_id)) ? false : true;
+        $is_logged_in = ($member_id !== null);
         if ($is_logged_in) {
             $member      = new Storage_Member();
             $member_name = $member->selectRecord(['name'], [['id', '=', $member_id]])['name'];
@@ -94,7 +94,7 @@ class Controller_Post extends Controller_Base
     public function delete()
     {
         $member_id    = $this->session_manager->getParam('member_id');
-        $is_logged_in = (is_null($member_id)) ? false : true;
+        $is_logged_in = ($member_id !== null);
 
         $post_id       = $this->getParam('post_id');
         $previous_page = $this->getParam('previous_page');
@@ -144,7 +144,7 @@ class Controller_Post extends Controller_Base
     public function edit()
     {
         $member_id    = $this->session_manager->getParam('member_id');
-        $is_logged_in = (is_null($member_id)) ? false : true;
+        $is_logged_in = ($member_id !== null);
 
         $post_id       = $this->getParam('post_id');
         $previous_page = $this->getParam('previous_page');
