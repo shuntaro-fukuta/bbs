1995b7037e224d98819f4b60bf965ac56d2eb16e
Hayatarou921 <Hayatarou921@gmail.com>
2020-01-10 20:01:43 +0900

    labelのミスを修正

--- html/admin/search_form.php
+++ html/admin/search_form.php
@@ -10,7 +10,7 @@
   <input type="radio" name="search_conditions[image]" value="without" <?php if (isset($search_conditions['image']) && $search_conditions['image'] === 'without') echo 'checked' ?>>without
   <input type="radio" name="search_conditions[image]" value="unspecified" <?php if (!isset($search_conditions['image']) || $search_conditions['image'] === 'unspecified') echo 'checked' ?>>unspecified
   <br>
-  <label for="post">Post : </label>
+  <label for="search_conditions[post]">Post : </label>
   <input type="radio" name="search_conditions[post]" value="on" <?php if (isset($search_conditions['post']) && $search_conditions['post'] === 'on') echo 'checked' ?>>on
   <input type="radio" name="search_conditions[post]" value="delete" <?php if (isset($search_conditions['post']) && $search_conditions['post'] === 'delete') echo 'checked' ?>>delete
   <input type="radio" name="search_conditions[post]" value="unspecified" <?php if (!isset($search_conditions['post']) || $search_conditions['post'] === 'unspecified') echo 'checked' ?>>unspecified
